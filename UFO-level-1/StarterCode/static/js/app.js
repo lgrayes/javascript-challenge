8547a6a5e5c3fcd88f2d5cb13435984915a98d95
lgrayes <lgrayes@gmail.com>
2020-12-07 11:37:07 -0600

    Upload data as table in html

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,55 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+// // Step 1: Loop Through `data` and console.log each ufo report object
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+  });
+
+// // Step 2:  Use d3 to append one table row `tr` for each ufo report object
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+  });
+
+// // Step 3:  Use `Object.entries` to console.log each ufo report value
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+  
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+    });
+  });
+
+  // // Step 4: Use d3 to append 1 cell per ufo report value
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+  
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+    });
+  });
+
+  // // Step 5: Use d3 to update each cell's text with
+// // ufo report values 
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
